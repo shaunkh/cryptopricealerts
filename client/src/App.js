2e7100cec8afe6978be8289215179006e6ec7e22
Shaun <shaun.khundker1@gmail.com>
2021-08-16 20:47:54 -0400

    full stack app with btc price, doesn't work yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState({prices: []});
+
+  useEffect(() => {
+    fetch("/prices")
+      .then((res) =>  {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("Error fetching data from server", err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cryptoasset Prices</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {this.state.prices.map(p => (
+              <tr key={p.currency}>
+                <td>{p.currency}</td>
+                <td>{p.price}</td>
+              </tr>
+            ))} */}
+        </tbody>
+      </table>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,25 +3,16 @@
 var express = require("express");
 var axios = require("axios");
 var app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function(request, response) {
-//   response.sendFile(__dirname + "/app/index.html");
-// });
-
-app.get("/prices", function(request, response) {
+app.get("/prices", function(req, res) {
   axios
-    .get("https://api.nomics.com/v1/prices?key=" + process.env.NOMICS_API_KEY)
+    .get("https://api.nomics.com/v1/currencies/ticker?key=" + process.env.NOMICS_API_KEY + "&ids=BTC")
     .then(resp => {
-      response.send(resp.data);
+      res.send(resp.data);
     })
     .catch(err => {
       console.log("Error fetching data from nomics", err);
